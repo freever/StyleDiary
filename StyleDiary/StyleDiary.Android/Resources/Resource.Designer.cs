88ae093194ca36753dcf3bb06200fc13fbebe7b4
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2017-10-26 13:44:02 +0200

    no message

--- StyleDiary/StyleDiary.Android/Resources/Resource.Designer.cs
+++ StyleDiary/StyleDiary.Android/Resources/Resource.Designer.cs
@@ -3894,13 +3894,7 @@ namespace StyleDiary.Droid
 			global::MxSync.Android.Resource.Drawable.abc_textfield_search_material = global::StyleDiary.Droid.Resource.Drawable.abc_textfield_search_material;
 			global::MxSync.Android.Resource.Drawable.abc_vector_test = global::StyleDiary.Droid.Resource.Drawable.abc_vector_test;
 			global::MxSync.Android.Resource.Drawable.avd_hide_password = global::StyleDiary.Droid.Resource.Drawable.avd_hide_password;
-			global::MxSync.Android.Resource.Drawable.avd_hide_password_1 = global::StyleDiary.Droid.Resource.Drawable.avd_hide_password_1;
-			global::MxSync.Android.Resource.Drawable.avd_hide_password_2 = global::StyleDiary.Droid.Resource.Drawable.avd_hide_password_2;
-			global::MxSync.Android.Resource.Drawable.avd_hide_password_3 = global::StyleDiary.Droid.Resource.Drawable.avd_hide_password_3;
 			global::MxSync.Android.Resource.Drawable.avd_show_password = global::StyleDiary.Droid.Resource.Drawable.avd_show_password;
-			global::MxSync.Android.Resource.Drawable.avd_show_password_1 = global::StyleDiary.Droid.Resource.Drawable.avd_show_password_1;
-			global::MxSync.Android.Resource.Drawable.avd_show_password_2 = global::StyleDiary.Droid.Resource.Drawable.avd_show_password_2;
-			global::MxSync.Android.Resource.Drawable.avd_show_password_3 = global::StyleDiary.Droid.Resource.Drawable.avd_show_password_3;
 			global::MxSync.Android.Resource.Drawable.design_bottom_navigation_item_background = global::StyleDiary.Droid.Resource.Drawable.design_bottom_navigation_item_background;
 			global::MxSync.Android.Resource.Drawable.design_fab_background = global::StyleDiary.Droid.Resource.Drawable.design_fab_background;
 			global::MxSync.Android.Resource.Drawable.design_ic_visibility = global::StyleDiary.Droid.Resource.Drawable.design_ic_visibility;
@@ -7614,26 +7608,8 @@ namespace StyleDiary.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f02010e
-			public const int avd_hide_password_1 = 2130837774;
-			
-			// aapt resource value: 0x7f02010f
-			public const int avd_hide_password_2 = 2130837775;
-			
-			// aapt resource value: 0x7f020110
-			public const int avd_hide_password_3 = 2130837776;
-			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
-			
-			// aapt resource value: 0x7f020111
-			public const int avd_show_password_1 = 2130837777;
-			
-			// aapt resource value: 0x7f020112
-			public const int avd_show_password_2 = 2130837778;
-			
-			// aapt resource value: 0x7f020113
-			public const int avd_show_password_3 = 2130837779;
 			
 			// aapt resource value: 0x7f020055
 			public const int design_bottom_navigation_item_background = 2130837589;
